3843d4b0b47963d94458b9966db9be1afb6ef4d6
Podstavin Aleksei <gwinbleid86@gmail.com>
2021-01-20 21:20:52 +0600

    add read/write file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,8 +21,26 @@ public class Main {
         };
 
 
+
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(products);
+
+//        try (FileWriter fw = new FileWriter("./products.txt")) {
+//            fw.write(gson.toJson(products));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        String json = "";
+        try (
+                FileReader fr = new FileReader("./products.txt");
+                Scanner scan = new Scanner(fr)
+        ) {
+            while (scan.hasNextLine()) {
+                json += scan.nextLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println(json);
 
         Product[] p = gson.fromJson(json, Product[].class);
